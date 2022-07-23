783a081d24691fae319cf44c47209a50efae67ed
huaji8 <81504466+huaji8@users.noreply.github.com>
2022-07-23 22:58:16 +0800

    Add files via upload

---
+++ js/main2.js
@@ -0,0 +1,39 @@
+(function () {
+
+  var bv = new Bideo();
+  bv.init({
+    // Video element
+    videoEl: document.querySelector('#background_video'),
+
+    // Container element
+    container: document.querySelector('body'),
+
+    // Resize
+    resize: true,
+
+    // autoplay: false,
+
+    isMobile: window.matchMedia('(max-width: 768px)').matches,
+
+    playButton: document.querySelector('#play'),
+    pauseButton: document.querySelector('#pause'),
+
+    // Array of objects containing the src and type
+    // of different video formats to add
+    src: [
+      {
+        src: 'https://webstatic.m ihoyo.com/upload/static-resource/2021/08/30/2124d8b2c9a3543660acf53acd16f83c_3148858834240991057.mp4',
+        type: 'video/mp4'
+      },
+      {
+        src: 'night.webm',
+        type: 'video/webm;codecs="vp8, vorbis"'
+      }
+    ],
+
+    // What to do once video loads (initial frame)
+    onLoad: function () {
+      document.querySelector('#video_cover').style.display = 'none';
+    }
+  });
+}());
